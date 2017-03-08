cee7f1b0bb8ebbb0c75e2cb90b20f4ce6d9c79aa
Zachary Schreiber <zachgives@gmail.com>
2017-03-08 16:37:25 -0500

    prep for final merge

---
+++ src/js/json.js
@@ -0,0 +1,67 @@
+var mpnData = {
+    "hipaa": {
+        "protected": "Yes",
+        "noticeUrl": "http://www.google.com"
+    },
+    "use": {
+        "companyOps": true,
+        "scientificResearch": true,
+        "marketing": true,
+        "development": true
+    },
+    "share": {
+        "developmentWithId": true,
+        "scientificResearchWithId": true,
+        "companyOpsNoId": false,
+        "scientificResearchNoId": false,
+        "otherWithId": true,
+        "otherDescriptionWithId": "To TEST INPUT",
+        "noneNoId": true
+    },
+    "sell": {
+        "dataWithId": "Yes",
+        "dataNoId": "Yes. With your permission,"
+    },
+    "store": {
+        "device": "Yes",
+        "external": "Yes"
+    },
+    "encryption": {
+        "device": "Yes, by default",
+        "server": "Yes, when you take certain steps",
+        "transmit": "No"
+    },
+    "privacy": {
+        "requestPermissions": "Yes",
+        "socialMedia": "No",
+        "other": false,
+        "microphone": true,
+        "location": false,
+        "photos": false,
+        "camera": true,
+        "health": true
+    },
+    "user": {
+        "hasOptions": "No",
+        "edit": true,
+        "share": true,
+        "delete": true
+    },
+    "deactivation": {
+        "action": "deleteOnRequest"
+    },
+    "policy": {
+        "notificationMethod": "We will email you ASAP"
+    },
+    "breach": {
+        "procedure": "Lock your account and email you"
+    },
+    "contact": {
+        "name": "TEST company",
+        "privacyPolicy": "link",
+        "onlineComments": "link",
+        "email": "test@test.com",
+        "phone": "(919)456-2345",
+        "address": "asdasdasdas"
+    }
+}
